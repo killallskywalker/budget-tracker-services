716532b48d76c73ecfa5b21dce83a3c9be842e65
KILLALLSKYWALKER <firdausdaus98@ymail.com>
2020-06-11 01:10:47 +0800

    add cors

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const transaction = require('./api/controller/transaction');
 const recurringPayment = require('./api/controller/recurringPayment');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(cors());
 app.use('/api/transaction-record', transaction);
 app.use('/api/recurring-payment', recurringPayment);
 
